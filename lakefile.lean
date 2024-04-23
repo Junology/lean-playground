1b242bb3f98b3c7502294eb5a50aae553d07d34b
Junology <jtachan@hotmail.com>
2024-04-23 15:29:50 +0900

    chore: fix lean-toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -4,14 +4,12 @@ open Lake DSL
 package «lean-playground» where
   -- add package configuration options here
 
-require std from git "https://github.com/leanprover/std4" @ "main"
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+require std from git "https://github.com/leanprover/std4" @ "v4.7.0"
+-- require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
 @[default_target]
 lean_lib «LeanPlayground» where
   -- add library configuration options here
-
-#print LeanExeConfig
 
 lean_exe «ArrayBench» where
   srcDir := "./Test"
